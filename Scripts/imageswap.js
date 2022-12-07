7c06070324c820753fbbede39dfe53e71611a5cc
Xalsier <81521840+Xalsier@users.noreply.github.com>
2022-12-07 17:33:53 -0500

    Create imageswap.js

---
+++ Scripts/imageswap.js
@@ -0,0 +1,16 @@
+var imageSwapped = false;
+var audio = document.getElementById("myAudio");
+
+function swapImage() {
+var img = document.getElementById("myImage");
+    if (imageSwapped) {
+      img.src = "../Assets/Unknown_Disc_1.png";
+      audio.pause();
+      imageSwapped = false;
+    } 
+    else {
+      img.src = "../Assets/Back_Home_Again.png";
+      audio.play();
+      imageSwapped = true;
+    }
+}
