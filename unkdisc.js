a6bb640ed7cc75646110838065eb3264fca3827b
Xalsier <81521840+Xalsier@users.noreply.github.com>
2022-12-06 21:49:09 -0500

    Update unkdisc.js

--- unkdisc.js
+++ unkdisc.js
@@ -29,30 +29,7 @@ imageContainer.addEventListener('click', () => {
   }
 });
 
-imageContainer.addEventListener('click', () => {
-  if (isPlaying) {
-    audio.pause();
-    firstImage.style.display = 'block';
-    audio.style.display = 'none';
-    isPlaying = false;
-  } else {
-    audio.play();
-    firstImage.parentNode.removeChild(firstImage);
-    const secondImage = document.createElement('img');
-    secondImage.src = 'Back_Home_Again.png';
-    secondImage.alt = 'Second Image';
-    secondImage.style.display = 'block';
-    secondImage.style.position = 'absolute';
-    secondImage.style.top = '0';
-    secondImage.style.left = '0';
-    secondImage.style.width = '100%';
-    secondImage.style.height = '100%';
-    secondImage.class = "spin";
-    imageContainer.appendChild(secondImage);
-    audio.style.display = 'block';
-    isPlaying = true;
-  }
-});imageContainer.addEventListener('touchstart', () => {
+imageContainer.addEventListener('touchstart', () => {
   if (isPlaying) {
     audio.pause();
     firstImage.style.display = 'block';
