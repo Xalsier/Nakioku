f65c0be805fe4fce134c728b041714bcd8a5be1b
Xalsier <81521840+Xalsier@users.noreply.github.com>
2022-12-07 16:55:21 -0500

    Update nav.js

--- nav.js
+++ nav.js
@@ -6,16 +6,3 @@ fetch('navbar.html')
     newelem.innerHTML = text;
     oldelem.parentNode.replaceChild(newelem,oldelem);
 })
-
-  // Get the current URL
-  var currentURL = location.pathname;
-
-  // Get the list of navigation items
-  var navItems = document.querySelectorAll('nav ul li a');
-
-  // Loop through the items and add a class to the one that matches the current URL
-  navItems.forEach(function(navItem) {
-    if (navItem.getAttribute('href') === currentURL) {
-      navItem.classList.add('active');
-    }
-  })
