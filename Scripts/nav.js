3f0c32a644247dd35534db4ce62725f4734a4475
Xalsier <81521840+Xalsier@users.noreply.github.com>
2022-12-08 06:04:01 -0500

    Update nav.js

--- Scripts/nav.js
+++ Scripts/nav.js
@@ -1,6 +1,22 @@
-fetch('../navbar.html')
-.then(res => res.text())
-.then(text => {
-    let oldelem = document.querySelector("script#replace_with_navbar");
-    oldelem.innerHTML = text;
-})
+const navDiv = document.createElement('div');
+navDiv.className = 'nav';
+
+const nav = document.createElement('nav');
+navDiv.appendChild(nav);
+
+const ul = document.createElement('ul');
+nav.appendChild(ul);
+
+const items = ['Gallery', 'Music', 'Calendar', 'Roadmap'];
+
+for (const item of items) {
+    const li = document.createElement('li');
+    ul.appendChild(li);
+
+    const a = document.createElement('a');
+    a.textContent = item;
+    a.href = `Pages/${item.toLowerCase()}.html`;
+    li.appendChild(a);
+}
+
+document.body.appendChild(navDiv);
