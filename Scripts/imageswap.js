2245342c74ef004dd3035e1ec58f7a97a7f3ad81
Xalsier <81521840+Xalsier@users.noreply.github.com>
2022-12-08 05:50:22 -0500

    Update imageswap.js

--- Scripts/imageswap.js
+++ Scripts/imageswap.js
@@ -4,12 +4,12 @@ var audio = document.getElementById("myAudio");
 function swapImage() {
 var img = document.getElementById("myImage");
     if (imageSwapped) {
-      img.src = "../Assets/Unknown_Disc_1.png";
+      img.src = "Assets/Unknown_Disc_1.png";
       audio.pause();
       imageSwapped = false;
     } 
     else {
-      img.src = "../Assets/Back_Home_Again.png";
+      img.src = "Assets/Back_Home_Again.png";
       audio.play();
       imageSwapped = true;
     }
