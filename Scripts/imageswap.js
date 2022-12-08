27a7d363e98755a020aad036a2c5c5c0d6f66ad6
Xalsier <81521840+Xalsier@users.noreply.github.com>
2022-12-08 06:43:49 -0500

    Update imageswap.js

--- Scripts/imageswap.js
+++ Scripts/imageswap.js
@@ -2,15 +2,27 @@ var imageSwapped = false;
 var audio = document.getElementById("myAudio");
 
 function swapImage() {
-var img = document.getElementById("myImage");
+    var img = document.getElementById("myImage");
     if (imageSwapped) {
-      img.src = "Assets/Unknown_Disc_1.png";
-      audio.pause();
-      imageSwapped = false;
+        img.src = "Assets/Unknown_Disc_1.png";
+        audio.pause();
+        imageSwapped = false;
+        stopSpin();
     } 
     else {
-      img.src = "Assets/Back_Home_Again.png";
-      audio.play();
-      imageSwapped = true;
+        img.src = "Assets/Back_Home_Again.png";
+        audio.play();
+        imageSwapped = true;
+        startSpin();
     }
+}
+
+function startSpin() {
+    var img = document.getElementById("myImage");
+    img.style.animation = "spinning infinite 20s linear";
+}
+
+function stopSpin() {
+    var img = document.getElementById("myImage");
+    img.style.animation = "";
 }
