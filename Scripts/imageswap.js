fcc69a17bd87a2955933e982f223e84a6982fefb
Xalsier <81521840+Xalsier@users.noreply.github.com>
2022-12-08 17:33:12 -0500

    Update imageswap.js

--- Scripts/imageswap.js
+++ Scripts/imageswap.js
@@ -1,16 +1,18 @@
-var imageSwapped = false;
-
 function swapImage() {
     console.log("Swapping image...");
     var audio = document.getElementById("myAudio");
     var img = document.getElementById("myImage");
+    img.style.transformOrigin = "center";
+    img.style.transition = "transform 0.5s ease-in-out";
     if (imageSwapped) {
+        img.style.transform = "";
         img.src = "Assets/Unknown_Disc_1.png";
         audio.pause();
         imageSwapped = false;
         stopSpin();
     } 
     else {
+        img.style.transform = "rotate(360deg)";
         img.src = "Assets/Back_Home_Again.png";
         console.log("Playing audio...");
         audio.play();
