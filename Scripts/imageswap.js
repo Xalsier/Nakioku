92cb41d1001d373a49a80fab54fa25d7b3a7a5f2
Xalsier <81521840+Xalsier@users.noreply.github.com>
2022-12-08 21:06:53 -0500

    Update imageswap.js

--- Scripts/imageswap.js
+++ Scripts/imageswap.js
@@ -1,3 +1,17 @@
+// Print the log messages to the debug console
+console.table(console.log.history);
+
+// Get the container where the logs will be displayed
+const container = document.querySelector('#console-logs');
+
+// Get the array of log messages
+const logs = console.table.history;
+
+// Loop through the log messages and add them to the container
+for (const log of logs) {
+  container.innerHTML += `<p>${log}</p>`;
+}
+
 var audioElement = document.getElementById("myAudio");
 var imageSwapped = false;
 
@@ -38,15 +52,4 @@ function stopSpin() {
     console.log("Stopping spin...");
     var img = document.getElementById("myImage");
     img.style.animation = "";
-}
-
-// Get the container where the logs will be displayed
-const container = document.querySelector('#console-logs');
-
-// Get the array of log messages
-const logs = console.log.history;
-
-// Loop through the log messages and add them to the container
-for (const log of logs) {
-  container.innerHTML += `<p>${log}</p>`;
 }
